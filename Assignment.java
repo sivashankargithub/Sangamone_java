079e1320fccc771745f2f082b8c723e85a51696f
Sivashankar C <144573155+sivashankargithub@users.noreply.github.com>
2023-10-10 07:48:32 +0530

    Add files via upload

--- Assignment.java
+++ Assignment.java
@@ -3,35 +3,34 @@ package com.sangamone;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Assignment {
 	public static void main(String args[]) throws FileNotFoundException {
 		List<String>list1=new ArrayList<>();
 		List<String>list2=new ArrayList<>();
-		List<String>list3=new ArrayList<>();
-		String info1="";
 		File f1=new File("data.txt");
 		Scanner sc = new Scanner(f1);
 		while(sc.hasNextLine()) {
-			list1.add(sc.nextLine());
+			String info1=sc.nextLine();
+			String[] words=info1.split(" ");
+			for(String word : words) {
+				list1.add(word);
+			}
 		}
+		//list2.add(list1.get(0));
+		//Collections.sort(list1);
 		System.out.println(list1);
-		System.out.println();
-		list2.add(list1.get(0));
+		for(String ele :list1) {
+			if(!list2.contains(ele)) {
+				list2.add(ele);
+			}
+		}
 		System.out.println(list2);
-		for(int i=1;i<list1.size();i++) {
-			list3.add(list2.get(0)+" "+list1.get(i));
-		}
-		System.out.println(list3);
-		System.out.println();
-		System.out.println(list1);
-		Collections.sort(list1);
-		System.out.println();
-		System.out.println(list1);
+		list2.sort(null);
+		//Collections.sort(list2);
+		System.out.println(list2);
+		sc.close();
 	}
 }
