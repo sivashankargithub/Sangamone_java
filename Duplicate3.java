4fd5bcc9423633d6a16e8f69c31201f18d5663e0
Sivashankar C <144573155+sivashankargithub@users.noreply.github.com>
2023-10-11 10:28:42 +0530

    Add files via upload

---
+++ Duplicate3.java
@@ -0,0 +1,33 @@
+package com.sangamone;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Duplicate3 {
+	public static void main(String[] args) {
+		System.out.println(duplicate1());
+	}
+	public static List<Integer> duplicate1()
+	{
+		List<Integer> list1=new ArrayList<>();
+		List<Integer> list2=new ArrayList<>();
+		list1.add(4);
+		list1.add(2);
+		list1.add(4);
+		list1.add(1);
+		list1.add(2);
+		list1.add(1);
+		
+		list2.addAll(list1);
+		for(int i=0;i<list2.size();i++) {
+			
+			for(int j=i+1;j<list2.size();j++) {
+				if(list2.get(i)==list2.get(j)) {
+					list2.remove(j);
+				}
+			}
+		}
+		return list2;
+		
+	}
+}
