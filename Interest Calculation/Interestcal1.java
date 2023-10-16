98322f722e3f1b71f65ba1c812c04365358a31a6
Sivashankar C <144573155+sivashankargithub@users.noreply.github.com>
2023-10-16 20:31:44 +0530

    Update Interestcal1.java

--- Interest Calculation/Interestcal1.java
+++ Interest Calculation/Interestcal1.java
@@ -1,13 +1,1 @@
-package com.sangamone;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
-public class Interestcal1 {
-	public static void main(String[] args) throws FileNotFoundException {
-		System.out.println();	
-	}
-}
