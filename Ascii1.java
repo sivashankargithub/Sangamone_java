d1f3f91845fe2d12994158f14135b01533bf6d6d
Sivashankar C <144573155+sivashankargithub@users.noreply.github.com>
2023-10-24 21:30:19 +0530

    Add files via upload

---
+++ Ascii1.java
@@ -0,0 +1,17 @@
+package com.sangamone;
+
+public class Ascii1 {
+
+	public static void main(String[] args) {
+		
+		for(char ui = 'A'; ui <= 'Z'; ui++) {
+			System.out.println(ui +"-"+(int) ui);
+		}
+
+
+		for(char li = 'a'; li <= 'z'; li++) {
+			System.out.println(li + "-" +(int)li);
+		}
+	}
+}
+	
