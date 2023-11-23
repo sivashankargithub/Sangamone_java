351d83d93cbe49620c3fe4e329f1ec48e4b5ee32
Sivashankar C <144573155+sivashankargithub@users.noreply.github.com>
2023-11-23 23:12:05 +0530

    Diamond
    Diamond

---
+++ Diamond/Diamond.java
@@ -0,0 +1,33 @@
+package com.sangamone;
+
+public class Diamond {
+
+
+    public static void main(String[] args) {
+        String str1 = "FUNWITHPROGRAMMINGISFUNFORALLOFUS";
+        int len = str1.length();
+        int len2 = (len / 2);
+        String s1 = "";
+        String spaceholder = " ";
+
+        // Upper part of the diamond
+        for (int j = 0; j < len2 + 1; j++) {
+            for (int i = 0; i < (len2 - j); i++) {
+                System.out.print(spaceholder);
+            }
+            s1 = str1.substring(0, len - 2 * (len2 - j));
+            System.out.print(s1);
+            System.out.println();
+        }
+
+        // Lower part of the diamond
+        for (int j = len2 - 1; j >= 0; j--) {
+            for (int i = 0; i < (len2 - j); i++) {
+                System.out.print(spaceholder);
+            }
+            s1 = str1.substring(0, len - 2 * (len2 - j));
+            System.out.print(s1);
+            System.out.println();
+        }
+    }
+}
