0bcbcc069d7ae75424c64df3fcdda017642485cb
Sivashankar C <144573155+sivashankargithub@users.noreply.github.com>
2023-11-27 23:59:43 +0530

    Add files via upload

---
+++ C46.java
@@ -0,0 +1,30 @@
+package com.sangamone.sangamone5;
+
+public class C46 {
+
+	public static void main(String[] args) {
+		byte bmin=Byte.MIN_VALUE;
+		byte bmax=Byte.MAX_VALUE;
+		
+		short smin=Short.MIN_VALUE;
+		short smax=Short.MAX_VALUE;
+		
+		int imin=Integer.MIN_VALUE;
+		int imax=Integer.MAX_VALUE;
+		
+		long lmin=Long.MIN_VALUE;
+		long lmax=Long.MAX_VALUE;
+		
+		System.out.println(bmin);
+		System.out.println(bmax+"\n");
+		System.out.println(smin);
+		System.out.println(smax+"\n");
+		System.out.println(imin);
+		System.out.println(imax+"\n");
+		System.out.println(lmin);
+		System.out.println(lmax);
+		
+
+	}
+
+}
