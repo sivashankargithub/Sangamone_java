2c52444fa65f03c47f033b22280b9988acef4eba
Sivashankar C <144573155+sivashankargithub@users.noreply.github.com>
2023-12-13 23:36:33 +0530

    String concatination
    String concatination

---
+++ Add1.java
@@ -0,0 +1,12 @@
+package com.sangamone;
+
+public class Add1 {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=5;
+		System.out.println("Addition :" +a+b);
+		System.out.println(a+b+" : is the addition of a and b");
+	}
+
+}
