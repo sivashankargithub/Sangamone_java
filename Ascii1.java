384dcc30cc8b5f04c7582de64bbdd0c84b568719
Sivashankar C <144573155+sivashankargithub@users.noreply.github.com>
2024-02-26 19:46:45 +0530

    Generating Ascii characters to String.
    Generating Ascii characters to String.

--- Ascii1.java
+++ Ascii1.java
@@ -1,17 +1,27 @@
 package com.sangamone;
 
+import java.util.ArrayList;
+
 public class Ascii1 {
-
 	public static void main(String[] args) {
+		char a='a';
+		String b=String.valueOf(a);
+		System.out.println(b);
+		ArrayList<String>list1=new ArrayList<>();
+		ArrayList<String>list2=new ArrayList<>();
+		for(char i='A';i<='Z';i++) {
+			list1.add(String.valueOf(i));
+		}
+		System.out.println(list1);
 		
-		for(char ui = 'A'; ui <= 'Z'; ui++) {
-			System.out.println(ui +"-"+(int) ui);
+		for(int i=1;i<26;i++) {
+			if(i<10) {
+				list2.add("0"+String.valueOf(i));
+			}
+			else {
+				list2.add(String.valueOf(i));
+			}
 		}
-
-
-		for(char li = 'a'; li <= 'z'; li++) {
-			System.out.println(li + "-" +(int)li);
-		}
+		System.out.println(list2);
 	}
 }
-	
