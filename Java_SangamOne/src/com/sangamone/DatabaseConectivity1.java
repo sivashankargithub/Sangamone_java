7dd30f9401058e6d589a3f1b63034a4a43d938f1
Sivashankar C <144573155+sivashankargithub@users.noreply.github.com>
2024-03-06 23:00:30 +0530

    Simple Java Database connectivity(JDBC)
    Simple Java Database connectivity(JDBC)

--- Java_SangamOne/src/com/sangamone/DatabaseConectivity1.java
+++ Java_SangamOne/src/com/sangamone/DatabaseConectivity1.java
@@ -3,6 +3,7 @@ package com.sangamone;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseConectivity1 {
@@ -17,11 +18,11 @@ public class DatabaseConectivity1 {
 			Statement stmt=con.createStatement();
 			ResultSet rs=stmt.executeQuery("select * from student");
 			while(rs.next()) {
-				System.out.println(rs.getInt(1)+" "+rs.getString(1)+" "+rs.getString(1));
+				System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
 			}
 			con.close();
 		}
-		catch(Exception e) {
+		catch(SQLException e) {
 			System.out.println(e);
 		}
 
